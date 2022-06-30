cbe2c929bd4792a9c4c0007da831b07814bbe8b2
ermias <ermiasgashaw7881@gmail.com>
2022-06-30 04:56:25 -0700

    addition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(len(sys.argv)-1))
     for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print(0)
+    else:
+        print(sum(list(map(int,sys.argv[1:]))))
