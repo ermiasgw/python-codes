923493da5a6d50d997f74795c51854b0f443e562
ermias <ermiasgashaw7881@gmail.com>
2022-07-08 10:07:13 -0700

    task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    if __name__ == "__main__":
-        def square_matrix_simple(matrix=[]):
-            return [x**2 for x in matrix]
+if __name__ == "__main__":
+    def square_matrix_simple(matrix=[]):
+        return [x**2 for x in matrix]
