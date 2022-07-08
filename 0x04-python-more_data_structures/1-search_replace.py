9ab823717987400a5e2ba1d6d3628ac7707713a0
ermias <ermiasgashaw7881@gmail.com>
2022-07-08 10:25:04 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [i for i in my_list if i != search else replace]
+    return [i if i != search else replace for i in my_list]
